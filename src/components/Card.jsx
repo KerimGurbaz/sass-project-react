19e9f30eea518e97191fe560f602eeebebfa1f9d
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-10-14 11:07:18 +0200

    card-style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 
+import cardStyle from "../scss/card.module.scss";
 const Card = ({ data }) => {
   return (
-    <div className="container">
+    <div className={cardStyle["container"]}>
       {data.map((item) => {
         const { id, job, img, name, comment } = item;
         return (
-          <div>
+          <div key={id} className={cardStyle["card"]}>
             <h1>{name}</h1>
             <h2>{job}</h2>
             <p>{comment}</p>
             <img src={img} alt="" />
-            <div>
-              <button>Small</button>
-              <button>Large</button>
+            <div className={cardStyle["btn-container"]}>
+              <button className={cardStyle["btn-container--small"]}>
+                Small
+              </button>
+              <button className={cardStyle["btn-container--large"]}>
+                Large
+              </button>
             </div>
           </div>
         );
